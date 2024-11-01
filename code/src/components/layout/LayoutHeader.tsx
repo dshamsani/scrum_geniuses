d4f06e1393c33bfb557f8636a6d2c4291caef8c4
dshamsani <shamsani.david@gmail.com>
2024-11-01 11:35:40 +0100

    feat: layour header transition

--- code/src/components/layout/LayoutHeader.tsx
+++ code/src/components/layout/LayoutHeader.tsx
@@ -1,23 +1,27 @@
-import type {FC} from "react"
+import type { FC } from "react";
 
-import RedLogo from '@/resources/red-logo.svg'
-import PhoneIcon from '@/resources/icons/phone.svg'
-
+import RedLogo from "@/resources/red-logo.svg";
+import PhoneIcon from "@/resources/icons/phone.svg";
 
 export const LayoutHeader: FC = () => {
-
   return (
-    <header className="h-[130px] flex items-center justify-between">
-      <img src={RedLogo} alt="Logo" className="w-fit" height={130}  />
-      <div className="flex items-center gap-12">
-        <span className="text-[15px] font-semibold">O nás</span>
-        <span className="text-[15px] font-semibold">Ceník</span>
-        <span className="text-[15px] font-semibold">Kontakty</span>
+    <header className='flex h-[130px] items-center justify-between'>
+      <img src={RedLogo} alt='Logo' className='w-fit' height={130} />
+      <div className='flex items-center gap-12 text-[15px] font-semibold'>
+        <span className='cursor-pointer border-b-2 border-transparent transition-all duration-150 hover:border-[#B00000]'>
+          O nás
+        </span>
+        <span className='cursor-pointer border-b-2 border-transparent transition-all duration-150 hover:border-[#B00000]'>
+          Ceník
+        </span>
+        <span className='cursor-pointer border-b-2 border-transparent transition-all duration-150 hover:border-[#B00000]'>
+          Kontakty
+        </span>
       </div>
-      <div className="border-l   flex items-center gap-4 pl-[14px]">
-        <img src={PhoneIcon} alt="Phone" width={20} height={20} />
-        <span className="font-semibold">+420*************</span>
+      <div className='flex items-center gap-4 border-l pl-[14px]'>
+        <img src={PhoneIcon} alt='Phone' width={20} height={20} />
+        <span className='font-semibold'>+420*************</span>
       </div>
     </header>
-  )
+  );
 };
