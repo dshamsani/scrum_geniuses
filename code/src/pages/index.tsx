96cf1c53f476f9ba5652aab23acccfcd48d48e59
David Choloyan <shamsani.david@gmail.com>
2024-11-14 14:06:48 +0100

    fix: unneeded code

--- code/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react";
-
-import { Layout } from "@/components/layout/Layout";
-
-export const Page: FC = () => {
-  return <Layout>
-    <></>
-  </Layout>
-};
--- code/src/types.d.ts
+++
@@ -1,5 +0,0 @@
-
-declare module '*.svg' {
-    const svg: string;
-    export default svg;
-}
