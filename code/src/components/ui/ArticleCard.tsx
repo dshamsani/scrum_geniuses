1f54fbf5d88203f85acb541f50c51d89cb356bcf
dshamsani <shamsani.david@gmail.com>
2024-12-16 01:02:00 +0100

    feat: archive page

--- code/src/components/ui/ArticleCard.tsx
+++ code/src/components/ui/ArticleCard.tsx
@@ -19,7 +19,7 @@ export const ArticleCard: FC<ArticleCardProps> = ({ title, description, date }) 
       <div className="shadow-card cursor-pointer flex justify-between flex-col max-w-[300px] h-[300px] aspect-square px-3 py-4 rounded-lg flex-grow hover:scale-105 transition-all duration-300 gap-3">
         <div className="flex flex-col gap-3">
           <h3 className=" font-semibold text-sm text-center self-center">{title}</h3>
-          <p className=" text-sm max-h-[200px]" dangerouslySetInnerHTML={{ __html: description.slice(0, 400) + "..." }}></p>
+          <p className=" text-sm max-h-[200px]" dangerouslySetInnerHTML={{ __html: description.slice(0, 300) + "..." }}></p>
         </div>
         <Button title="Cist vice" withoutTransform onClick={() => setOpen(true)} />
       </div>
--- code/src/pages/Archive/Archive.tsx
+++ code/src/pages/Archive/Archive.tsx
@@ -1,21 +1,44 @@
 import type { FC } from "react";
+import type { Article } from "@/types/article";
 
 import { PageHeader } from "@/components/ui/PageHeader";
-import { PageSection } from "@/components/ui/PageSection";
+import { ArticleCard } from "@/components/ui/ArticleCard";
+
+import { useEffect, useState } from "react";
 
 export const Archive: FC = () => {
+  const [data, setData] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      const res = await fetch(`https://admin.scrumgeniuses.cloud/items/Clanek?filter[status][_eq]=archiv`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      return res;
+    };
+
+    handleFetch()
+      .then((data) => data.json())
+      .then((data) => setData(data.data));
+  }, []);
+
   return (
     <main className="pt-[4px]">
       <PageHeader
         title="Archiv"
         description="Vítejte v archivu Scrum Geniuses. Přinášíme vám bohatou sbírku minulých článků, příběhů a inspirativních materiálů. Prozkoumejte naše předchozí publikace a nechte se inspirovat nápady, které přetrvávají. Ať už hledáte konkrétní téma nebo si chcete připomenout oblíbený obsah, náš archiv je tu pro vás."
       />
-
-      <PageSection
-        title="Vydani časopisu"
-        wrapperClassname="max-w-[800px] w-full flex flex-col gap-3"
-        content={<p>Zatím nebyl nahrán žádný casopis</p>}
-      />
+      <div className="flex w-full p-desktop justify-center pb-10">
+        <div className="max-w-[1350px] w-full flex flex-wrap gap-10">
+          {(data ?? []).map(({ id, nazev, text_clanku, date_created }) => (
+            <ArticleCard key={id} title={nazev} description={text_clanku} date={date_created} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
